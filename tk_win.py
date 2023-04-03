85f56491c64d39cfa771c5ef9b3d804f4c07dafa
Alex <temper8@gmail.com>
2023-04-03 12:01:25 +0300

    Update tk_win.py

--- tk_win.py
+++ tk_win.py
@@ -5,8 +5,10 @@ def show_tk_win():
     root.title("Tkinter app")
     root.geometry("300x250")    
     
-    label = tk.Label(text="Hello") 
-    label.pack()    
+    label = tk.Label(text="Hello", 
+                     width=20, height=5,
+                     bg="lightgreen") 
+    label.pack(expand=1)    
  
     root.mainloop()
 
